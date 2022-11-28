b308ed8e150a44fae9530daa5878982b3a039438
AliA-Mohamad <ali.mohamad.uc@gmail.com>
2022-11-28 16:31:39 -0300

    28/11/2022 16:30

--- src/App.js
+++ src/App.js
@@ -5,6 +5,44 @@ import Time from './componentes/Time';
 
 
 function App() {
+
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9'
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E9F8FF'
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria: 'A6D157',
+      corSecundaria: 'F0F8E2'
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: 'E06B69',
+      corSecundaria: 'FDE7E8'
+    },
+    {
+      nome: 'UX e Design',
+      corPrimaria: 'DB6EBF',
+      corSecundaria: 'FAE9F5'
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: 'FFBA05',
+      corSecundaria: 'FFF5D9'
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: 'FF8A29',
+      corSecundaria: 'FFEEDF'
+    }
+  ]
   
   const [colaboradores, setColaboradores] = useState([])
 
@@ -17,10 +55,9 @@ function App() {
     <div>
       <Banner />
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorCadastrado(colaborador)}/>
-      <Time nome="Programação"/>
-      <Time nome="Frontend"/>
-      <Time nome="Data Science"/>
       
+      {times.map(time => <Time key={time.nome} nome={time.nome}/>)}
+
     </div>
   );
 }
